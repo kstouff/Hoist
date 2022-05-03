ff60cb0df78a6452d9b5a945ccdef385db85b434
Kyle Stouffer <kylejstouffer@gmail.com>
2022-05-03 15:31:41 -0700

    super ninjas

--- Ninja.js
+++ Ninja.js
@@ -18,8 +18,27 @@ class Ninja {
     
 }
 
-const ninja1 = new Ninja("Hyabusa", 40);
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+class Sensei extends Ninja {
+    constructor(name){
+        super(name, 200)
+        
+        this.wisdom = 10;
+    }
+
+    speakWisdom(wisdom){
+        this.drinkSake();
+        console.log(wisdom);
+        
+    }
+}
+
+// const ninja1 = new Ninja("Hyabusa", 40);
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.showStats();
+superSensei.speakWisdom("There is no spoon");
+superSensei.showStats();
